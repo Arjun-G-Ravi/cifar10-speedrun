f75f64044da39327f373ce18fb78bc3ce647f629
Arjun-G-Ravi <arjungravi1@gmail.com>
2025-08-29 08:57:55 +0530

    Patch

--- 1-base-neural-network/base-nn.py
+++ 1-base-neural-network/base-nn.py
@@ -5,22 +5,23 @@ import torch.nn as nn
 import time
 import torch.nn.init as init
 
-torch.set_float32_matmul_precision('high') # uses TF32
+# torch.set_float32_matmul_precision('high') # uses TF32
 torch.manual_seed(42)
 torch.cuda.manual_seed_all(42)
 
 # -----
 # CONFIG
-bs = 512
+bs = 1024 * 2
 lr = 1e-3
 # -----
+device = 'cuda'
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)), # these are  mean and std of cifar10 dataset over 3 color channels(this have massive role)
 
 ])
 train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=bs, shuffle=True, num_workers=2, pin_memory=True) # pin_memory loads thing in RAM(making cpu-to-gpu transfer fast), drop_last is to prevent shape issue at last datapoint
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=bs, shuffle=True, num_workers=8, pin_memory=True) # pin_memory loads thing in RAM(making cpu-to-gpu transfer fast), drop_last is to prevent shape issue at last datapoint
 
 test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False,download=True, transform=transform)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=bs, shuffle=False, num_workers=2, pin_memory=True) # 
@@ -30,13 +31,13 @@ class NeuralNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Linear(3*32*32, 1024),
+            nn.Linear(3*32*32, 512),
             nn.ELU(),
             nn.Dropout(.3),
-            nn.Linear(1024, 1024),
+            nn.Linear(512, 256),
             nn.ELU(),
-            nn.Dropout(.2),
-            nn.Linear(1024, 10),
+            nn.Dropout(.3),
+            nn.Linear(256, 10),
         )
         self.model.apply(self._init_weights)
 
@@ -52,7 +53,7 @@ class NeuralNet(nn.Module):
                 init.constant_(m.bias, 0)
 
 
-model = NeuralNet().to('cuda')
+model = NeuralNet().to(device)
 model.compile()
 
 loss_function = nn.CrossEntropyLoss()
@@ -63,9 +64,9 @@ def train_epoch():
     running_loss = 0
     num_batches = 0
 
-    for i,(x,y) in enumerate(train_loader):
+    for step,(x,y) in enumerate(train_loader):
         optimizer.zero_grad()
-        x, y = x.to('cuda', non_blocking=True), y.to('cuda') # non-blocking prevents cpu to gpu data transfer blocks(and make code faster)
+        x, y = x.to(device, non_blocking=True), y.to(device) # non-blocking prevents cpu to gpu data transfer blocks(and make code faster)
         y_pred = model(x)
         loss = loss_function(y_pred, y)
         running_loss += loss
@@ -76,12 +77,11 @@ def train_epoch():
 
 def test_model():
     model.eval()
-    print('\n-----')
     with torch.no_grad():
         corrrect_pred = 0
         total_pred = 0
         for i,(x,y) in enumerate(train_loader):
-            x, y = x.to('cuda', non_blocking=True), y.to('cuda') 
+            x, y = x.to(device, non_blocking=True), y.to(device) 
             y_pred = model(x)
             y_pred_class = torch.argmax(y_pred, dim=1)
             corrrect_pred += (y == y_pred_class).sum()
@@ -91,7 +91,7 @@ def test_model():
         corrrect_pred = 0
         total_pred = 0
         for i,(x,y) in enumerate(test_loader):
-            x, y = x.to('cuda', non_blocking=True), y.to('cuda') 
+            x, y = x.to(device, non_blocking=True), y.to(device) 
             y_pred = model(x)
             y_pred_class = torch.argmax(y_pred, dim=1)
             corrrect_pred += (y == y_pred_class).sum()
@@ -100,14 +100,15 @@ def test_model():
     
 
 start = time.time()
-test_model()
-for i in range(50):
+print('Epoch:')
+for i in range(30):
     print(i+1,'->', end=' ')
     train_epoch()
-    if i%10 == 0:
+    if (i+1)%10 == 0 and i > 1:
+        print('-----')
         test_model()
-        print('\n-----')
+        print('-----\n')
 test_model()
-print('\n-----')
+print('-----')
 end = time.time()
 print(f"Training time: {end - start:.2f}s")
